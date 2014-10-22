e374317f695c2434e5aa6e6a6fcfbe94fed3f40d
Igor Kur <kur91@mail.ru>
2014-10-22 17:59:51 +0300

    add map to companies

--- app/models/company.js
+++ app/models/company.js
@@ -4,6 +4,12 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   logo: DS.attr('string'),
   products: DS.hasMany('product', {async: true}),
+  lat:   DS.attr('number'),
+  long:  DS.attr('number'),
+  zoom:  DS.attr('number'),
   updated_at: DS.attr('date'),
-  created_at: DS.attr('date')
+  created_at: DS.attr('date'),
+
+  asGoogleMap: ['lat', 'long', 'zoom']
+
 });
