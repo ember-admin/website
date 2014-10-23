c12f1fb35b88f1904bfd1b149fef22f9f5f65f79
Igor Kur <kur91@mail.ru>
2014-10-23 16:15:46 +0300

    create company logo adapter

--- app/models/company.js
+++ app/models/company.js
@@ -2,14 +2,16 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  logo: DS.attr('string'),
   products: DS.hasMany('product', {async: true}),
   lat:   DS.attr('number'),
   long:  DS.attr('number'),
   zoom:  DS.attr('number'),
   updated_at: DS.attr('date'),
   created_at: DS.attr('date'),
+  logo:  DS.belongsTo('logo'),
 
-  asGoogleMap: ['lat', 'long', 'zoom']
+  asGoogleMap: ['lat', 'long', 'zoom'],
+  fileuploads: ["logo"],
+  formFields: ['title', 'lat', 'long', 'zoom']
 
 });
